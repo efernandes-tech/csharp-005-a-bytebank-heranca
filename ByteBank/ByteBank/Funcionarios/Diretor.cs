a46532728edd35ee56c30538f390a2a70e81a675
edersonlrf <edersonluis.rf@gmail.com>
2019-02-06 14:39:41 -0200

    A2 a palavra reservada base

--- ByteBank/ByteBank/Funcionarios/Diretor.cs
+++ ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -8,7 +8,7 @@ namespace ByteBank.Funcionarios
     {
         public override double GetBonificacao()
         {
-            return Salario;
+            return Salario + base.GetBonificacao();
         }
     }
 }
